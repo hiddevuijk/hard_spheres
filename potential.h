8fbafb24a45b295acb82f6c06011dfef02b33090
hiddevuijk <hdvuijk@gmail.com>
2019-12-11 09:21:44 +0100

    added diameter to potiential parameters

--- potential.h
+++ potential.h
@@ -6,19 +6,21 @@
 class Potential {
 public:
     Potential( double rhs, double rco, double beta)
-        : rhs(rhs), rhs_sq(rhs*rhs), rco(rco), beta(beta) {}
+        : rhs(rhs), dhs(2*rhs), dhs_sq(4*rhs*rhs),
+          rhs_sq(rhs*rhs), rco(rco), beta(beta)
+        {}
 
     
     double U( const XYZ& r1, const XYZ& r2, double L) { return 0;}
     bool get_overlap(const XYZ& r1, const XYZ& r2, double L) ;
 
-    double rhs, rhs_sq, rco, beta;
+    double rhs,dhs, dhs_sq, rhs_sq, rco, beta;
 };
 
 
 bool Potential::get_overlap(const XYZ& r1, const XYZ& r2, double L)
 {
-    if( xyz::dist_sq_pbc(r1,r2,L) < 4*rhs_sq ) return true;
+    if( xyz::dist_sq_pbc(r1,r2,L) < dhs_sq ) return true;
     else return false;
 }
 
