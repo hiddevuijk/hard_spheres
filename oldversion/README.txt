e6f2e0f9a5439aa650625c89e210d9ebb5dd2ed3
hiddevuijk <hdvuijk@gmail.com>
2019-12-12 14:29:02 +0100

    delete and put back again

--- oldversion/README.txt
+++
@@ -1,7 +0,0 @@
-TO DO:
-
-
--- Implement check overlap in update naighbours
--- dynamic step size
--- max neighbours < 10, so decrese max neighbours from N to 10
--- implement psi6
--- plot.py
+++ plot.py
@@ -5,27 +5,24 @@ from sys import exit
 N = 1000
 L = 20.
 rho = N/(L**3)
+rho = 0.8
 
 def g1(rho):
     eta = np.pi*rho/6
     return (1+0.5*eta)/( (1-eta)**2 )
 
-sim = np.loadtxt("sim_data_rho0.8_1.8_1.1111.dat")
-simx = sim[:,0]
-simg = sim[:,1]
 
 gr = np.loadtxt("gr.dat")
 r = gr[:,0]
 g = gr[:,1]
 
-gr1 = np.loadtxt("gr_rc3.dat")
+gr1 = np.loadtxt("gr_old.dat")
 r1 = gr[:,0]
 g1 = gr[:,1]
 
+plt.plot(r1,g1, color='blue')
 plt.plot(r,g, color='black')
-#plt.plot(r1,g1+0.1, color='blue')
-plt.plot(simx,simg, color='red')
-#plt.scatter(1,g1(rho) )
+plt.scatter(1,g1(rho) )
 plt.title(rho)
 plt.show()
 
